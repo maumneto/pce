3af6eff74d9d02402608c27c861e36071dc88474
Maurício Moreira Neto <maumneto@gmail.com>
2019-08-15 08:09:15 -0300

    Primeiro Commit da Repositório
    Material da Disciplina.

---
+++ js/code.js
@@ -0,0 +1,38 @@
+function return_site(){
+    document.location.href = "https://maumneto.github.io/mauricioneto/";
+}
+
+function send_mail(){
+    ConfirmBoxEmail();
+}
+
+function ButtonMouseOver(element){
+    element.style.color = "#4169E1";
+    element.style.backgroundColor = "#E0FFFF";
+}
+
+function ButtonMouseOut(element){
+    element.style.color = "#000000";
+    element.style.backgroundColor = "white";
+}
+
+function BigImgOver(element){
+    element.style.height = "48px";
+    element.style.width = "48px";
+}
+
+function BigImgOut(element){
+    element.style.height = "30px";
+    element.style.width = "30px";
+}
+
+function ConfirmBoxEmail(){
+    var return_value;
+    return_value = confirm("Você pretende mandar um E-mail?");
+    
+    if(return_value == true){
+        document.location.href = "mailto:maumneto@gmail.com";
+    } else {
+        return;
+    }
+}
